18533ca16674c20aa114ddffe39b52da75128b8b
suneel129 <abbireddi_suneel@srmuniv.edu.in>
2017-07-15 15:09:44 +0530

    Create greaternum.py

---
+++ greaternum.py
@@ -0,0 +1,12 @@
+print ("enter A")
+a=input()
+print ("enter B")
+b=input()
+print ("enter C")
+c=input()
+if (a>b and a>c):
+	print ("A is greater")
+elif(b>c):
+	print("b is greater")
+else:
+	print ("C is greater")
